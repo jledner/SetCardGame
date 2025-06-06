46fe2bc328cab736d0beb9af200f022721a70937
Jake Ledner <jake.ledner@sphere.tech>
2025-06-06 17:17:17 -0400

    got card checking logic a bit further, but still need to clean up CardContent == SetElement piece

--- Set/SetGame.swift
+++ Set/SetGame.swift
@@ -10,12 +10,13 @@ import Foundation
 struct SetGame<CardContent> {
     private(set) var deck: Array<Card>
     private(set) var cardsInPlay: [Card] // currently dealt cards
+    private(set) var selectedCards: [Card]
 
     
     init(numberOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         deck = []
         cardsInPlay = []
-        
+        selectedCards = []
         for index in 0..<numberOfCards {
             let content = cardContentFactory(index)
             deck.append(Card(id: index, content: content))
@@ -26,8 +27,12 @@ struct SetGame<CardContent> {
         if let chosenIndex = cardsInPlay.firstIndex(where: { $0.id == card.id
         }) {
             cardsInPlay[chosenIndex].isSelected.toggle()
+            selectedCards.append(cardsInPlay[chosenIndex])
+            if selectedCards.count == 3 {
+            }
         }
     }
+    
     
     mutating func deal(_ count: Int){
         let next = deck.prefix(count)
@@ -39,5 +44,32 @@ struct SetGame<CardContent> {
         var id: Int
         var isSelected = false
         let content: CardContent
+        
+        var debugDescription: String {
+            "\(id) - \(isSelected ? "selected" : "" )"
+        }
+    }
+}
+
+extension SetGame where CardContent == SetElement {
+    mutating func choose(with card: Card) {
+        if let chosenIndex = cardsInPlay.firstIndex(where: { $0.id == card.id }) {
+            cardsInPlay[chosenIndex].isSelected.toggle()
+            selectedCards.append(cardsInPlay[chosenIndex])
+            if selectedCards.count >= 3 {
+                checkForSet(with: selectedCards)
+            }
+        }
+    }
+
+    mutating func checkForSet(with selection: [Card]) {
+        print("✅ Specialized checkForSet called")
+        let referenceShape = selection[0].content.shape
+        for givenCard in selection {
+            print(givenCard.content.shape)
+            if givenCard.content.shape == referenceShape {
+                cardsInPlay.removeAll { $0.id == givenCard.id }
+            }
+        }
     }
 }
